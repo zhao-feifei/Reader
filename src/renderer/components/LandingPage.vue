b1df5c8ccc1d72ed874d1b36f0ddd3b09c8e1ea5
zhao-feifei <398983914@qq.com>
2021-09-24 20:29:11 +0800

    引入AntdUI完成

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="wrapper"></div>
+  <!-- <div id="wrapper"></div> -->
+  <a-button type="primary">
+    Primary
+  </a-button>
 </template>
 
 <script>
 import SystemInformation from './LandingPage/SystemInformation'
-
+// import { Button } from 'ant-design-vue'
 export default {
   name: 'landing-page',
+  // components: {
+  //   Button,
+  // },
 }
 </script>
 
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,10 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import Ant from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.use(Ant)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
@@ -14,5 +18,5 @@ new Vue({
   components: { App },
   router,
   store,
-  template: '<App/>'
+  template: '<App/>',
 }).$mount('#app')
