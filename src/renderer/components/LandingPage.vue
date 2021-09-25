811bb9e7c7054c3dd6b0b49a366f377e49f34a87
zhao-feifei <398983914@qq.com>
2021-09-25 20:18:36 +0800

    接口调用完成

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,18 +1,42 @@
 <template>
-  <!-- <div id="wrapper"></div> -->
-  <a-button type="primary">
-    Primary
-  </a-button>
+  <div id="wrapper">
+    <div class="search">
+      <a-input-search
+        placeholder="input search text"
+        enter-button
+        @search="onSearch"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import SystemInformation from './LandingPage/SystemInformation'
+
 // import { Button } from 'ant-design-vue'
 export default {
   name: 'landing-page',
   // components: {
   //   Button,
   // },
+  data() {
+    return {
+      searchList: [],
+    }
+  },
+  methods: {
+    async onSearch(value) {
+      let res = await this.$http.get(
+        // 'https://weread.qq.com/web/search/search_suggest?keyword=${' +
+        //   value +
+        //   '}&count=40'
+        `https://weread.qq.com/web/search/search_suggest?keyword=${value}&count=40`
+      )
+      this.searchList = res.data.records
+      console.log(res)
+      console.log(this.searchList)
+    },
+  },
 }
 </script>
 
